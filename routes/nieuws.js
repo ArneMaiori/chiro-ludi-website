a32ae52dc60c6f243ea8c576adb295f774487df1
ArneM-2365112 <arne.maiori@student.uhasselt.be>
2025-12-01 15:19:45 +0100

    Sitemap change #3

--- routes/nieuws.js
+++ routes/nieuws.js
@@ -63,6 +63,7 @@ router.get('/:id', async (req, res) => {
       post: nieuwsItem,
       isAdmin: req.session?.isAdmin,
       activePage: activePage, 
+      pageTitle: nieuwsItem.title,
       backLink: backLink,
       backText: backText
     });
